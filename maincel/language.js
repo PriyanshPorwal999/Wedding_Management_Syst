2c7749fb69c3035b27fb7ef5757a1811b933f8e8
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-04-08 01:19:05 +0530

    Updated language.js file

---
+++ maincel/language.js
@@ -0,0 +1,8 @@
+// This is the google language translator for multi lingual website 
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    { pageLanguage: "en" },
+    "google_translate_element"
+  );
+}
