5a4a7535065b6d9fd9e80355fe48cb5af40d9e22
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-06-15 01:40:17 +0530

    Added service section features

---
+++ src/component/Services/images.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./images.css";
+
+function Images() {
+    return (
+        <div className="images-container">
+            <img
+                src="/src/assets/budget image.png"
+                className="budget-image"
+                alt="Budget Management"
+            />
+        </div>   
+            
+    );
+}
+
+export default Images;
---
+++ src/component/Services/services.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./services.css";
+import Images from "./images.jsx";
+
+const Services = () => {
+  return (
+    <div className="services-container">
+      <div className="services-header"> 
+        <Images />
+      </div>
+      
+      <div className="services-content">
+        <h2 className="budget_1">Budget Management</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
