b82836177288c0665ddfa95ba83569c0068387ff
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-06-18 00:26:25 +0530

    Added button component

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./navbar.css";
 import Logo from "./logo.jsx";
-
+import Button from "./Button/button.jsx";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-                <Logo />
-                <button className="vendor">Vendor</button>
-                <button className="photography">Photography</button>
-                <button className="catering">Catering</button>
-                <button className="login">Login</button>
-
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <Logo />
+      <Button label="Home" />
+      <Button label="Vendor" />
+      <Button label="Photography" />
+      <Button label="Catering" />
+      <Button label="Login" />
+    </nav>
+  );
 };
 
 export default Navbar;
